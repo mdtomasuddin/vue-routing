989e51cad616a19776347d7e588f03ddb1f05bbb
MDTOMSDUDDIN <mdtomasuddin1@gmail.com>
2025-02-24 22:04:23 +0600

    route|prams pass

---
+++ src/components/PersonView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { useRoute } from 'vue-router';
+
+const route=useRoute();
+const name=route.params.name;
+</script>
+
+<template>
+
+    <div>
+        <h1>this is a person page </h1>
+        <p>Name : {{ name }}</p>
+        <p>directly access Name : {{ $route.params.name }}</p>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/ContactView.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+
+</script>
+ 
+<template>
+<h1>this is a contact page !</h1>
+
+</template>
+ 
+<style scoped>
+ 
+</style>
